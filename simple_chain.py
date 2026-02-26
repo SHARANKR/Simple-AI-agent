7d5763d29f3c13e46c2fd0e40eb946b63e895052
SHARANKR <sharankr798@gmail.com>
2026-02-26 16:45:02 +0530

    4. Changed to gemini due to insufficient balannce

--- simple_chain.py
+++ simple_chain.py
@@ -1,4 +1,5 @@
 from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
@@ -10,10 +11,12 @@ prompt = PromptTemplate(
     input_variables= ["topic"]
 )
 
-model = ChatOpenAI()
+model = ChatGoogleGenerativeAI(model = "gemini-3-flash-preview")
 
 parser = StrOutputParser()
 
 chain = prompt | model | parser
 
 result = chain.invoke({"topic":"vegetables"})
+
+print(result)
