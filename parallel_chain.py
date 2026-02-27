cdabd30dfe957a3450da7cfd50eb01392bb9b2b5
SHARANKR <sharankr798@gmail.com>
2026-02-27 17:06:37 +0530

    8. Missed {} in older version, now added to runnableparallel

--- parallel_chain.py
+++ parallel_chain.py
@@ -26,9 +26,20 @@ model = ChatGoogleGenerativeAI(model = "gemini-3-flash-preview")
 
 parser = StrOutputParser()
 
-chain = prompt_1 | model | parser | prompt_2 | model | parser
+parallel_chain = RunnableParallel({
+    "notes": prompt_1 | model | parser,
+    "quiz": prompt_2 | model | parser
+})
 
-result = chain.invoke({"topic":"vegetables"})
+merge_chain = prompt_3 | model | parser 
+
+chain = parallel_chain | merge_chain
+
+text = """
+The Transformer architecture, introduced in the groundbreaking paper Attention Is All You Need by researchers at Google, has fundamentally transformed the field of Artificial Intelligence and Natural Language Processing. Unlike traditional sequential models such as RNN and LSTM, Transformers rely on a powerful mechanism called self-attention, which allows the model to understand relationships between words regardless of their distance in a sentence while also enabling parallel processing, making training significantly faster and more efficient. The architecture typically consists of components such as token embeddings, positional encoding to preserve word order, multi-head self-attention to capture different contextual relationships, feed-forward neural networks for deeper representation learning, and residual connections with layer normalization to stabilize training. This architecture has become the backbone of many modern AI systems, powering models like BERT, GPT, and T5, which are widely used in applications such as conversational AI, intelligent search, code generation, AI assistants, and autonomous AI agents, making Transformers one of the most important innovations driving today’s AI revolution.
+"""
+
+result = chain.invoke({"topic":text})
 
 print(result)
 
